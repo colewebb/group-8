bdaefd92e3efa03556b54997d82e9b5a015151b7
Logan Smith <logansmith283@aggiemail.usu.edu>
2021-03-11 19:55:16 -0700

    Got the api working for basic lot info, still need to add spots and other things.

--- Apps/parking_api/events/migrations/0002_events.py
+++ Apps/parking_api/events/migrations/0002_events.py
@@ -30,6 +30,33 @@ def populate_db(apps, schema_editor):
                address = '600 E 1150 N, North Logan, UT 84341')
     e2.save()
 
+    # sample lots
+    Lot = apps.get_model('events', 'Lot')
+
+    l1 = Lot(name='East Stadium',
+            address='900 E 1000 N Logan UT, 84341',
+            openTime=datetime.datetime.now(),
+            closeTime=datetime.datetime.now(),
+            capacityActual=500,
+            capacityMax=500)
+    l1.save()
+
+    l2 = Lot(name='Blue Terrace',
+            address='850 E Aggie BLVD Logan, UT 84321',
+            openTime=datetime.datetime.now(),
+            closeTime=datetime.datetime.now(),
+            capacityActual=300,
+            capacityMax=300)
+    l2.save()
+
+
+    #sample spots
+    #Spot = apps.get_model('events', 'Spot')
+
+    #s1 = Spot(lot=l1, size='small', cost=7.50)
+    #s1.save()
+
+
 class Migration(migrations.Migration):
     dependencies = [
         ('events', '0001_initial'),
--- Apps/parking_api/events/urls.py
+++ Apps/parking_api/events/urls.py
@@ -6,4 +6,5 @@ app_name = 'events'
 urlpatterns = [
     path('users/', views.getUsers, name='getUsers'),
     path('events/', views.getEvents, name='getEvents'),
+    path('lots/', views.getLots, name='getLots'),
 ]
