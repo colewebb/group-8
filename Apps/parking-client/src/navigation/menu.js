7cc1a4a4dacf88511bf3053c4c635e4b2bf10359
Ausitn Reeve <austin.reeve@aggiemail.usu.edu>
2021-03-13 03:04:06 -0700

    menu updates

--- Apps/parking-client/src/navigation/menu.js
+++ Apps/parking-client/src/navigation/menu.js
@@ -16,11 +16,23 @@ export default function Menu(props) {
         </div>
       </div>
       <div id="mySidenav" class="sidenav" style={{width: menuWidth}}>
+        <div class="credits-container">
+          <div class="credits-container-top">
+            <h2 class="name-text">Austin Reeve</h2>
+          </div>
+          <h2 class="value-text">$34.00</h2>
+          <h2 class="balance-text">Balance</h2>
+          <div />
+        </div>
         <a class="closebtn" onClick={() => openMenu(menuWidth = 0)}>&times;</a>
         <a href="/account">Account</a>
         <a href="/reservations">Reservations</a>
         <a href="/faq">FAQ</a>
-        <a href="/login">Logout</a>
+        <div class="logout-button-center">
+          <div class="logout-button">
+            <a class="logout-button-text" href="/login">Logout</a>
+          </div>
+        </div>
       </div>
     </nav>
   );
