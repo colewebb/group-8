176bb70f7c4e27671051b1e32ee99d85af1b2509
Cole Webb <cole.webb@outlook.com>
2021-03-24 20:00:13 -0600

    Got things working

--- Apps/parking_api/lotowners/views.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render
-from api.models import *
-
-
-def index(request):
-    return render(request, 'lotowners/index.html', {})
--- Apps/parking_api/parking_api/urls.py
+++ Apps/parking_api/parking_api/urls.py
@@ -21,5 +21,5 @@ urlpatterns = [
     url(r'^admin/', admin.site.urls),
     path('api/', include('api.urls')),
     path('api-auth/', include('rest_framework.urls')),
-    path('lotowners/', include('lotowners.urls')),
+    path('lotOwners/', include('lotOwners.urls'))
 ]
