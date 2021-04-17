a71e72356260433367c3bc139aeaab40d89e9e62
Cole Webb <cole.webb@outlook.com>
2021-04-17 10:26:00 -0600

    Finished lot attendant features
    The features here do no require authentication, as they do not include information not already publicly available.

--- Apps/parking_api/attendant/urls.py
+++ Apps/parking_api/attendant/urls.py
@@ -3,9 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('confirm', views.confirm, name="confirm"),
-    path('deny', views.deny, name="deny"),
-    path('login', views.login, name="login"),
-    path('logout', views.logout, name="logout"),
-    path('manual/<int:lot_id>', views.manual, name="manual")
+    path('get/<int:reservation_id>', views.get, name="get"),
+    path('confirm/<int:reservation_id>', views.confirm, name="confirm"),
+    path('deny/', views.deny, name="deny"),
+    path('manual/', views.manual, name="manual")
 ]
--- Apps/parking_api/attendant/views.py
+++ Apps/parking_api/attendant/views.py
@@ -1,53 +1,32 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
-from django.contrib.auth import authenticate, login as auth_login, logout as auth_logout
 from api.models import Lot, Reservation
 from .forms import *
 
 def index(request):
-    if not request.user.is_authenticated:
-        return HttpResponseRedirect("./login")
-    lots = Lot.objects.filter(owner=request.user)
-    return render(request, "attendant/index.html", {'lots': lots, 'user': request.user})
+    return redirect("./manual")
 
-def confirm(request):
-    if not request.user.is_authenticated:
-        return redirect('./')
-    return render(request, "attendant/confirm.html", {'user': request.user})
+def confirm(request, reservation_id):
+    reservation = Reservation.objects.get(pk=reservation_id)
+    return render(request, "attendant/confirm.html", {'reservation': reservation})
 
 def deny(request):
-    if not request.user.is_authenticated:
-        return redirect('./')
-    return render(request, "attendant/deny.html", {'user': request.user})
+    return render(request, "attendant/deny.html")
 
-def login(request):
-    if request.user.is_authenticated:
-        return redirect('./')
-    if request.method == "GET":
-        form = Login()
-        return render(request, 'attendant/login.html', {'form': form})
-    else:
-        user = authenticate(request, username=request.POST['username'], password=request.POST['password'])
-        if user is not None:
-            auth_login(request, user)
-            return redirect('./')
-        else:
-            return render(request, 'attendant/logout.html')
-
-def logout(request):
-    auth_logout(request)
-    return render(request, 'attendant/logout.html')
-
-def manual(request, lot_id):
-    if not request.user.is_authenticated:
-        return redirect('./')
+def manual(request):
     if request.method == "GET":
         form = ManualInput()
-        lot = Lot.objects.get(pk=lot_id)
-        return render(request, 'attendant/manual.html', {'user': request.user, 'form': form, 'lot': lot})
+        return render(request, 'attendant/manual.html', {'form': form})
     else:
-        reservation = Reservation.objects.get(pk=request.POST['reservationID'])
-        if reservation.lot == lot:
-            return redirect("../confirm.html")
-        else:
-            return redirect("../deny.html")
+        try:
+            reservation = Reservation.objects.get(pk=request.POST['reservationID'])
+            return redirect("../confirm/" + str(reservation.id))
+        except Reservation.DoesNotExist:
+            return redirect("../deny/")
+
+def get(request, reservation_id):
+    try:
+        reservation = Reservation.objects.get(pk=reservation_id)
+        return redirect("../confirm/" + str(reservation.id))
+    except Reservation.DoesNotExist:
+        return redirect("../deny/")
